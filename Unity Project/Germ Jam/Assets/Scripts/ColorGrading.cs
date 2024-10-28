f8303f1e5f2b64da1c0cc66037c2de5ec2b86f0d
HoppXR <brtl.hopper@gmail.com>
2024-10-27 21:59:27 -0400

    Imported LUTs

---
+++ Unity Project/Germ Jam/Assets/Scripts/ColorGrading.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class ColorGrading : MonoBehaviour
+{
+    [SerializeField] private Material[] lutMaterials;
+    private int _index = 0;
+    
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Debug.Log("OnRenderImage called");
+        
+        if (lutMaterials == null) return;
+        
+        Graphics.Blit(source, destination, CurrentLut(_index));
+    }
+
+    private Material CurrentLut(int index)
+    {
+        return lutMaterials[index];
+    }
+
+    public void NoLut()
+    {
+        _index = 0;
+    }
+
+    public void WarmLut()
+    {
+        _index = 1;
+    }
+
+    public void ColdLut()
+    {
+        _index = 2;
+    }
+
+    public void Custom1Lut()
+    {
+        _index = 3;
+    }
+
+    public void Custom2Lut()
+    {
+        _index = 4;
+    }
+}
